8bc2720d7b7f13a5d105daf73272485b75083ac1
JcHummingBird <84508372+JcHummingBird@users.noreply.github.com>
2024-04-17 08:27:47 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+
+// Change the code below this line.
+const Card = (props) => (
+  <li>
+    <img src={props.link} alt={props.alt} />
+    <h3>{props.description}</h3>
+  </li>
+);
+// Change the code above this line.
+
+// Change the code below this line.
+const App = () => (
+  <div>
+    <h1>Encyclopedia</h1>
+    <ul>
+      {/* Card 1 */}
+      <Card
+        link="https://codefinity-content-media.s3.eu-west-1.amazonaws.com/code-1/react/introduction-to-react/sun.png"
+        alt="Sun shines brightly."
+        description="The sun shone brightly, casting a warm glow across the tranquil beach."
+      />
+
+      {/* Card 2 */}
+      <Card
+        link="https://codefinity-content-media.s3.eu-west-1.amazonaws.com/code-1/react/introduction-to-react/mountain.png"
+        alt="Climbing up the mountain."
+        description="The hiker gazed in awe at the majestic mountain towering before them."
+      />
+    </ul>
+  </div>
+);
+// Change the code above this line.
+
+root.render(<App />);
